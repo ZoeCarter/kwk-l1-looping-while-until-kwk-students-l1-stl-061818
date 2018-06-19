6649993442eee9f0e7c71a7625679952cbbc2c69
ZoeCarter <zcarter-konate@micds.org>
2018-06-19 19:28:31 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -5,11 +5,7 @@ def levitation_force
   levitation_force = 6
   
   while counter < 10 
-    puts "To the right, to the right, to the right, to the right"
-    puts "To the left, to the left, to the left, to the left"
-    puts "Now kick, now kick, now kick, now kick"
-    puts "Now walk it by yourself, now walk it by yourself"
-    puts ""
+    puts "
   
   counter = counter + 1 
     puts "#{counter} move(s) completed!!"
