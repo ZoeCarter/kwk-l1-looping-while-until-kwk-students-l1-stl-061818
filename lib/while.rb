43a69d9d27610eaae665e51b28a4ef128d131818
ZoeCarter <zcarter-konate@micds.org>
2018-06-19 19:27:50 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,23 +1,23 @@
 def using_while
 	levitation_force = 6
-#While Loop 
-# def cupid_shuffle 
-#   counter = 0
+While Loop 
+def cupid_shuffle 
+  counter = 0
   
-#   while counter < 10 
-#     puts "To the right, to the right, to the right, to the right"
-#     puts "To the left, to the left, to the left, to the left"
-#     puts "Now kick, now kick, now kick, now kick"
-#     puts "Now walk it by yourself, now walk it by yourself"
-#     puts ""
+  while counter < 10 
+    puts "To the right, to the right, to the right, to the right"
+    puts "To the left, to the left, to the left, to the left"
+    puts "Now kick, now kick, now kick, now kick"
+    puts "Now walk it by yourself, now walk it by yourself"
+    puts ""
   
-#   counter = counter + 1 
-#     puts "#{counter} move(s) completed!!"
-#   end 
+  counter = counter + 1 
+    puts "#{counter} move(s) completed!!"
+  end 
   
-# end 
+end 
 
-# cupid_shuffle 
+cupid_shuffle 
 	
 end
 
